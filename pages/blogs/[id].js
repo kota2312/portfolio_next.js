15ddeb6083091ef00863b0b7aec23d66dccbc06c
Kota Takahashi <kota23122553@gmail.com>
2024-12-12 14:51:26 +0900

    デプロイ修正4

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -1,3 +1,5 @@
+import React from "react";
+import Link from "next/link";
 import { client } from "../../libs/client";
 import parse from 'html-react-parser';
 import { formatDate } from "../../utility/dateformat";
