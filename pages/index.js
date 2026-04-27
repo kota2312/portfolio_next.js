9c707331c9ada8653c13c9b7e3c414b832fcc26a
kota2312 <kota23122553@gmail.com>
2026-04-27 21:21:52 +0900

    Add horizontal landing experience

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { initScrollReveal } from "@/utility/scrollReveal";
-import Link from "next/link";
 import fs from "fs";
 import path from "path";
 import { client } from "../libs/client";
@@ -19,62 +18,78 @@ import BasicSlider from "@/components/BasicSlider";
 import MovieBg from "@/components/MovieBg";
 
 export default function Home({ images, blogs }) {
+  const horizontalRef = useRef(null);
+
   useEffect(() => {
-    // ScrollRevealの初期化を呼び出す
     initScrollReveal();
   }, []);
 
+  const handleHorizontalWheel = (event) => {
+    if (window.innerWidth <= 768 || !horizontalRef.current) {
+      return;
+    }
+
+    event.preventDefault();
+    horizontalRef.current.scrollLeft += event.deltaY + event.deltaX;
+  };
+
   return (
-    <div>
+    <div className="hp_horizontalSite">
       <Head />
       <Header />
       <MovieBg />
-      <section id="mainVisual" className="bl_mainVisual">
-        <Jumbotron images={images} />
-      </section>
-      <section id="aboutUs" className="bl_aboutUs">
-        <div className="ly_cont">
-          <Heading sectionId="aboutUs" />
-          <Media sectionId="aboutUs" />
-        </div>
-      </section>
-      <section id="blog" className="bl_blog">
-        <div className="ly_cont">
-          <Heading sectionId="blog" />
-          <div className="bl_blog_card">
-            <BasicSlider blogs={blogs} />
+      <main
+        ref={horizontalRef}
+        className="hp_horizontalPage"
+        onWheel={handleHorizontalWheel}
+      >
+        <section id="mainVisual" className="bl_mainVisual hp_panel hp_panel__hero">
+          <Jumbotron images={images} />
+        </section>
+        <section id="aboutUs" className="bl_aboutUs hp_panel hp_panel__about">
+          <div className="ly_cont">
+            <Heading sectionId="aboutUs" />
+            <Media sectionId="aboutUs" />
           </div>
-        </div>
-      </section>
-      <section id="works" className="bl_works hp_bgBase hp_spikes">
-        <div className="ly_cont">
-          <Heading sectionId="works" />
-          <TabNavigation sectionId="works" />
-        </div>
-      </section>
-      <section id="certification" className="bl_certification">
-        <div className="ly_cont">
-          <Heading sectionId="certification" />
-          <Card sectionId="certification" />
-        </div>
-      </section>
-      <section id="skill" className="bl_skill hp_bgBase hp_spikes">
-        <div className="ly_cont">
-          <Heading sectionId="skill" />
-          <Chart sectionId="skill" />
-        </div>
-      </section>
-      <section id="contact" className="bl_contact">
-        <div className="ly_cont">
-          <Heading sectionId="contact" />
-          <ContactForm />
-        </div>
-      </section>
-      <section id="cta" className="bl_cta-wrap hp_bgCta">
-        <div className="ly_cont ly_cont__cta">
-          <Cta />
-        </div>
-      </section>
+        </section>
+        <section id="blog" className="bl_blog hp_panel hp_panel__blog">
+          <div className="ly_cont">
+            <Heading sectionId="blog" />
+            <div className="bl_blog_card">
+              <BasicSlider blogs={blogs} />
+            </div>
+          </div>
+        </section>
+        <section id="works" className="bl_works hp_bgBase hp_spikes hp_panel hp_panel__works">
+          <div className="ly_cont">
+            <Heading sectionId="works" />
+            <TabNavigation sectionId="works" />
+          </div>
+        </section>
+        <section id="certification" className="bl_certification hp_panel hp_panel__certification">
+          <div className="ly_cont">
+            <Heading sectionId="certification" />
+            <Card sectionId="certification" />
+          </div>
+        </section>
+        <section id="skill" className="bl_skill hp_bgBase hp_spikes hp_panel hp_panel__skill">
+          <div className="ly_cont">
+            <Heading sectionId="skill" />
+            <Chart sectionId="skill" />
+          </div>
+        </section>
+        <section id="contact" className="bl_contact hp_panel hp_panel__contact">
+          <div className="ly_cont">
+            <Heading sectionId="contact" />
+            <ContactForm />
+          </div>
+        </section>
+        <section id="cta" className="bl_cta-wrap hp_bgCta hp_panel hp_panel__cta">
+          <div className="ly_cont ly_cont__cta">
+            <Cta />
+          </div>
+        </section>
+      </main>
       <Footer />
     </div>
   );
@@ -82,7 +97,7 @@ export default function Home({ images, blogs }) {
 
 export const getStaticProps = async () => {
   const imgDir = path.join(process.cwd(), "public", "img", "top", "slider");
-  const files = fs.readdirSync(imgDir).filter(file => /\.(png|jpe?g|gif)$/i.test(file));
+  const files = fs.readdirSync(imgDir).filter((file) => /\.(png|jpe?g|gif)$/i.test(file));
 
   const data = await client.get({ endpoint: "blogs" });
 
